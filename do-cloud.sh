0a6449f31646f1f06dcb1c73480c3c6dc1fe366a
Anthony Townsend <amt3@nyu.edu>
2022-08-14 19:00:00 -0400

    re-deployed but not sure the template fully works

--- do-cloud.sh
+++ do-cloud.sh
@@ -1,5 +1,6 @@
 sam build --use-container &&
 sam deploy \
     --stack-name BusObservatoryAPI \
-    --s3-bucket busobservatory-api \
+    --s3-bucket busobservatory \
+    --s3-prefix _deployed_code/BusObservatoryAPI \
     --capabilities CAPABILITY_IAM
