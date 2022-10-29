974af124290114f6c9fd1bec894e2be5ddfa4ff5
Anthony Townsend <amt3@nyu.edu>
2022-10-29 13:39:55 -0400

    first test deploy to ECS

--- stack.py
+++ stack.py
@@ -4,6 +4,7 @@ from constructs import Construct
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_iam as iam
 import aws_cdk.aws_ecs as ecs
+import aws_cdk.aws_secretsmanager as sm
 import aws_cdk.aws_ecs_patterns as ecs_patterns
 
 class BusObservatoryAPI(Stack):
@@ -42,32 +43,15 @@ class BusObservatoryAPI(Stack):
             task_image_options=image,
         )
         
-        #TODO: get the service role produced by the above self.ecs_service
-            
-        print(self.ecs_service)
-        self.ecs_service.task_definition.add_to_task_role_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonAthenaFullAccess"))
+        #Add the policies we need to the ECS Task Role
         
-        # Traceback (most recent call last):
-        # File "<string>", line 1, in <module>
-        # File "/Users/anthonytownsend/Dropbox/Desktop/code/cornell_tech/BusObservatory-API/.venv/lib/python3.9/site-packages/aws_cdk/aws_ecs/__init__.py", line 31711, in add_to_task_role_policy
-        #     check_type(argname="argument statement", value=statement, expected_type=type_hints["statement"])
-        # File "/Users/anthonytownsend/Dropbox/Desktop/code/cornell_tech/BusObservatory-API/.venv/lib/python3.9/site-packages/typeguard/__init__.py", line 785, in check_type
-        #     raise TypeError(
-        # TypeError: type of argument statement must be aws_cdk.aws_iam.PolicyStatement; got jsii._reference_map.InterfaceDynamicProxy instead
-
+        self.ecs_service.task_definition.task_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonAthenaFullAccess"))
+        # self.ecs_service.task_definition.task_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AWSGlueConsoleFullAccess"))
+        # self.ecs_service.task_definition.task_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("S3FullAccess"))
         
-        
-        
-        
-        #TODO: attach policies to whatever role is produced like " BusObservatoryAPIServiceTaskDefExecutionRole7D7F55E6"
-        # by self.ecs_service
-
-        # service_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonAthenaFullAccess"))
-        # ? service_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AWSGlueConsoleFullAccess"))
-        # ? service_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonAthenaFullAccess"))
-
-        #     - AWSSecretsManagerGetSecretValuePolicy:
-        #         SecretArn: 'arn:aws:secretsmanager:us-east-1:870747888580:secret:auth0_api.busobservatory.org-Mx25l5'
-
-        # Attach a Managed Policy to an IAM Role after Role Creation #
-        # https://bobbyhadz.com/blog/aws-cdk-iam-role#attach-a-managed-policy-to-an-iam-role-after-role-creation
+        # get the secret and grant access to the ECS Task Role
+        secret = sm.Secret.from_secret_attributes(self, "ImportedSecret",
+            secret_complete_arn="arn:aws:secretsmanager:us-east-1:870747888580:secret:auth0_api.busobservatory.org-Mx25l5"
+            )
+        secret.grant_read(self.ecs_service.task_definition.task_role)
+       
